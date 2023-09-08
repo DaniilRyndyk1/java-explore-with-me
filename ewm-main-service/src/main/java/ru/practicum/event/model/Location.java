54fc4ade3e0c662aed0f8db2080fd647c2a37ab0
DaniilRyndyk1 <dannysladkiy@yandex.ru>
2023-09-08 17:24:09 +0300

    Location.java entity

---
+++ ewm-main-service/src/main/java/ru/practicum/event/model/Location.java
@@ -0,0 +1,29 @@
+package ru.practicum.event.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "location")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "lat", nullable = false)
+    private float lat;
+
+    @NotNull
+    @Column(name = "lon", nullable = false)
+    private float lon;
+}
