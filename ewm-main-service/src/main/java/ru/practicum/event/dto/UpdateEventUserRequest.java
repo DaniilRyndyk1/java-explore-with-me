2ab571ec5573a260a2aaa101c4ec605b2496740f
DaniilRyndyk1 <dannysladkiy@yandex.ru>
2023-09-13 15:38:00 +0300

    Fix update request valid

--- ewm-main-service/src/main/java/ru/practicum/event/dto/UpdateEventUserRequest.java
+++ ewm-main-service/src/main/java/ru/practicum/event/dto/UpdateEventUserRequest.java
@@ -6,19 +6,16 @@ import lombok.NoArgsConstructor;
 import ru.practicum.event.enums.EventStateAction;
 import ru.practicum.location.dto.LocationDto;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class UpdateEventUserRequest {
-    @NotBlank
     @Size(min = 20, max = 2000)
     private String annotation;
     private Long category;
 
-    @NotBlank
     @Size(min = 20, max = 7000)
     private String description;
 
@@ -31,7 +28,6 @@ public class UpdateEventUserRequest {
     private Boolean requestModeration;
     private EventStateAction stateAction;
 
-    @NotBlank
     @Size(min = 3, max = 120)
     private String title;
 }
